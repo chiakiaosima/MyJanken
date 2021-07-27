dd0ac5921a7425d0c542cb9749f20745bfbd5001
chiakiaosima <starsrock5937@gmail.com>
2021-07-28 07:55:21 +0900

    グーの画像の実装

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
+        //グー画像を指定
         Image("gu")
+            //リサイズを指定
+            .resizable()
+            //画面内に収まるように、アスペクト比(縦横比)を維持する指定
+            .aspectRatio(contentMode: .fill)
     }
 }
 
