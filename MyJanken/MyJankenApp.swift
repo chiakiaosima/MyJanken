4e42ec90636c73da77ad700c970a6e546c004316
chiakiaosima <starsrock5937@gmail.com>
2021-07-27 15:29:27 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by 青島智明 on 2021/07/27.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
